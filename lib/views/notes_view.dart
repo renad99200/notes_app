da03484c95810c8110dbb0ab2a7f7fdd261b763b
renad99200 <rynadmad14@gmail.com>
2025-09-22 07:31:12 -0700

    add border reduis to bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -9,7 +9,9 @@ class NotesView extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: (){
-        showModalBottomSheet(context: context, builder: (context){
+        showModalBottomSheet(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)
+        )
+            ,context: context, builder: (context){
           return const AddNoteButtomSheet();
 
         }
--- lib/views/widgets/add_notebottusheet.dart
+++ lib/views/widgets/add_notebottusheet.dart
@@ -5,6 +5,9 @@ class AddNoteButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+
+
+    );
   }
 }
