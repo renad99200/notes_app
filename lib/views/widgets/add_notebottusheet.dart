2eea79749622aff0df1809c83bd80547014443e1
renad99200 <rynadmad14@gmail.com>
2025-09-25 12:56:17 -0700

    create custom text field

--- lib/views/widgets/add_notebottusheet.dart
+++ lib/views/widgets/add_notebottusheet.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/cupertino.dart';
 
+import 'custom_text_field.dart';
+
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-
-
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 32,),
+          CustomTextField()
+        ],
+      ),
     );
   }
 }
---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:flutter/material.dart';
+import '../../constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: KPrimaryColor,
+      decoration: InputDecoration(
+        hintText: "Title",
+        hintStyle: const TextStyle(
+          color: KPrimaryColor,
+        ),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(KPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([Color? color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
+  }
+}
+
